f67248382d19ad199e91cd32c46aca1c7e74ee4e
Alexander Jung <a.jung@lancs.ac.uk>
2019-05-27 12:54:12 +0100

    Fix #2

--- xen.sh
+++ xen.sh
@@ -183,7 +183,7 @@ case "$COMMAND" in
         make -j$(getconf _NPROCESSORS_ONLN) install
 
         update-grub
-        sed -i 's/GRUB_DEFAULT=0/GRUB_DEFAULT=3/g' /etc/default/grub
+        sed -i 's/GRUB_DEFAULT=[0-9]/GRUB_DEFAULT=3/' /etc/default/grub
 
         echo >> /etc/network/interfaces <<EOF
 auto xenbr0
